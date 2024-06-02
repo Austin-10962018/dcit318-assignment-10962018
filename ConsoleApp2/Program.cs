2debf9013840c97a7cd8e1589ad322448efe5606
Austin Atsu <102976207+Austin-10962018@users.noreply.github.com>
2024-06-02 23:43:13 +0000

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter your age: ");
+        int age = GetValidAge();
+
+        double ticketPrice = CalculateTicketPrice(age);
+
+        Console.WriteLine("The ticket price is: GHC" + ticketPrice);
+    }
+
+    static int GetValidAge()
+    {
+        int age;
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out age) && age >= 0)
+            {
+                return age;
+            }
+            else
+            {
+                Console.Write("Invalid input. Please enter a valid age: ");
+            }
+        }
+    }
+
+    static double CalculateTicketPrice(int age)
+    {
+        const double regularPrice = 10.0;
+        const double discountedPrice = 7.0;
+
+        if (age <= 12 || age >= 65)
+        {
+            return discountedPrice;
+        }
+        else
+        {
+            return regularPrice;
+        }
+    }
+}
---
+++ TriangleTypeId/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the length of the 1st side: ");
+        double side1 = GetPositiveDouble();
+
+        Console.Write("Enter the length of the 2nd side: ");
+        double side2 = GetPositiveDouble();
+
+        Console.Write("Enter the length of the 3rd side: ");
+        double side3 = GetPositiveDouble();
+
+        string triangleType = GetTriangleType(side1, side2, side3);
+
+        Console.WriteLine("This means your triangle is a/an " + triangleType +" triangle.");
+    }
+
+    static double GetPositiveDouble()
+    {
+        double result;
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out result) && result > 0)
+            {
+                return result;
+            }
+            else
+            {
+                Console.Write("Invalid input. Please enter a positive number: ");
+            }
+        }
+    }
+
+    static string GetTriangleType(double side1, double side2, double side3)
+    {
+        if (side1 == side2 && side2 == side3)
+        {
+            return "Equilateral";
+        }
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
+        {
+            return "Isosceles";
+        }
+        else
+        {
+            return "Scalene";
+        }
+    }
+}
