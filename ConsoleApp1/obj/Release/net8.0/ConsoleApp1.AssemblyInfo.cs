cb84f0d19658e9a1894b994ca53fcddb6280f5fc
Austin Atsu <102976207+Austin-10962018@users.noreply.github.com>
2024-06-02 23:39:41 +0000

    Add files via upload

--- ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d973c40d5f3cb427a50743187e32d2af06026c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
